30902bcb08720feddb37c5085e10084c9ae83e56
spyflow <javier.ferrada2201@alumnos.ubiobio.cl>
2026-04-03 15:49:07 -0300

    Comment out KneesDev link in Footer component
    Comment out the Link component for KneesDev in the footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="w-full bg-[#080808] text-white z-0 text-base flex justify-center items-center px-8 py-4">
             <div className="w-full max-w-[1700px] flex justify-between items-center">
-                <Link href="https://kneesdev.github.io" target="_blank">&copy; {new Date().getFullYear()} KneesDev</Link>
+                <!-- <Link href="https://kneesdev.github.io" target="_blank">&copy; {new Date().getFullYear()} KneesDev</Link> -->
                 <p className="cursor-default">Not affiliated with Moonsworth or Lunar Client</p>
             </div>
         </footer>
